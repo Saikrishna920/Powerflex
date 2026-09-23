067932f78b855231885f8d1c5752d9f4034ef48e
Saikrishna920 <saikrishna.tarani@gmail.com>
2026-09-24 05:26:11 +0530

    Modified test_ordering code

--- framework/pytest_features/test_ordering.py
+++ framework/pytest_features/test_ordering.py
@@ -1,19 +1,21 @@
 # Need plugin : pytest-order-------->pip install pytest-order
 # Even though the order of the functions are different, it will follow based on specified order.
+# Here, I have installed one more Plugin "pytest-ordering" --->pip install pytest-ordering
+
 import pytest
-@pytest.mark.order(4)
+@pytest.mark.run(order=4)
 def test_1():
-    print("This is test1")
+    print("This is test1 Tarani")
     assert True
-@pytest.mark.order(3)
+@pytest.mark.run(order=3)
 def test_2():
-    print("This is test2")
+    print("This is test2 Krishna")
     assert True
-@pytest.mark.order(1)
+@pytest.mark.run(order=1)
 def test_3():
-    print("This is test3")
+    print("This is test3 Lakshmi")
     assert True
-@pytest.mark.order(2)
+@pytest.mark.run(order=2)
 def test_4():
-    print("This is test4")
+    print("This is test4 Sai")
     assert True
